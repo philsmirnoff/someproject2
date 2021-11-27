30312717d53fcfa9cc7a13005bc79ee551ea6285
Ilfir Yakupov <philsmirnoff@gmail.com>
2021-11-26 21:40:00 -0500

    added timer to the main container

--- src/App.js
+++ src/App.js
@@ -22,7 +22,12 @@ function App() {
   ].reverse();
   return (
     <div className="app">
-      <div className="main">main</div>
+      <div className="main">
+        <div className="top">
+          <div className="timer">30</div>
+        </div>
+        <div className="bottom">question and answers</div>
+      </div>
       <div className="pyramid">
         <ul className="moneyList">
           {moneyPyramid.map((m) => (
