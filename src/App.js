c2bfd605190c4d3eb0c456e92fb16297ff402d43
Ilfir Yakupov <philsmirnoff@gmail.com>
2021-11-28 22:22:06 -0500

    added animation on answers section

--- src/App.js
+++ src/App.js
@@ -3,7 +3,79 @@ import Trivia from './components/Trivia';
 import './App.css';
 
 function App() {
-  const [questionNumber, setQuestionNumber] = useState(5);
+  const [questionNumber, setQuestionNumber] = useState(1);
+  const [stop, setStop] = useState(false);
+
+  const data = [
+    {
+      id: 1,
+      question: "What is a Bitcoin?",
+      answers: [
+        {
+          text: "Penny",
+          correct: false,
+        },
+        {
+          text: "Dime",
+          correct: false,
+        },
+        {
+          text: "Quater",
+          correct: false,
+        },
+        {
+          text: "Cryptocurrency",
+          correct: true,
+        },
+      ],
+    },
+    {
+      id: 2,
+      question: "The first digital currency that allows users to send and receive money, without the interference of a central bank or government. Instead, a network of thousands of peers is controlling the transactions; a decentralized system.",
+      answers: [
+        {
+          text: "Dogecoin",
+          correct: false,
+        },
+        {
+          text: "Litecoin",
+          correct: false,
+        },
+        {
+          text: "Bitcoin",
+          correct: true,
+        },
+        {
+          text: "Shiba Inu",
+          correct: false,
+        },
+      ],
+    },
+    {
+      id: 3,
+      question: "Bitcoin was invented by an anonymous person or group named",
+      answers: [
+        {
+          text: "Satoshi Nakamoto",
+          correct: true,
+        },
+        {
+          text: "Jack Sparrow",
+          correct: false,
+        },
+        {
+          text: "Wolf of Wall Street",
+          correct: false,
+        },
+        {
+          text: "Elon Mask",
+          correct: false,
+        },
+      ],
+    },
+  ];
+
+
   const moneyPyramid = [
     { id: 1, amount: "$ 100" },
     { id: 2, amount: "$ 200" },
@@ -28,7 +100,12 @@ function App() {
           <div className="timer">30</div>
         </div>
         <div className="bottom">
-          <Trivia />
+          <Trivia
+           data={data}
+           setStop={setStop}
+           questionNumber={questionNumber}
+           setQuestionNumber={setQuestionNumber}
+           />
         </div>
       </div>
       <div className="pyramid">
--- src/components/Trivia.js
+++ src/components/Trivia.js
@@ -1,14 +1,50 @@
-import React from 'react'
+import React from 'react';
+import { useEffect, useState } from 'react';
 
-function Trivia() {
+function Trivia({
+  data,
+  setStop,
+  questionNumber,
+  setQuestionNumber
+}) {
+  const [question, setQuestion] = useState(null);
+  const [selectedAnswer, setSelectedAnswer] = useState(null);
+  const [className, setClassName] = useState("answer");
+
+  useEffect(() => {
+      setQuestion(data[questionNumber - 1])
+  }, [data, questionNumber]);
+
+  const delay = (duration, callback) => {
+    setTimeout(() => {
+      callback();
+    }, duration);
+  };
+
+  const handleClick = (a) => {
+    setSelectedAnswer(a);
+    setClassName("answer active");
+    delay(3000, () => setClassName(a.correct ? "answer correct" : "answer wrong"));
+    delay(5000, () =>
+    {
+      if (a.correct) {
+        setQuestionNumber((prev) => prev + 1);
+        setSelectedAnswer(null)
+        } else {
+        setStop(true);
+      }
+    })
+  };
+
   return (
     <div className="trivia">
-      <div className="question">What's the Bitcoin?</div>
+      <div className="question">{question?.question}</div>
       <div className="answers">
-          <div className="answer">Penny</div>
-          <div className="answer">Dime</div>
-          <div className="answer">Quarter</div>
-          <div className="answer">Cryptocurrency</div>
+        {question?.answers.map((a) => (
+          <div className={selectedAnswer === a ? className : "answer"} onClick={() => handleClick(a)}>
+            {a.text}
+            </div>
+        ))}
       </div>
     </div>
   )
