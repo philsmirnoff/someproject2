e8cc52f04085971fae303216c35606257c04b7fe
Ilfir Yakupov <philsmirnoff@gmail.com>
2021-11-29 22:26:47 -0500

    moved questions to its  seperate file

---
+++ src/data/data.js
@@ -0,0 +1,332 @@
+export const data = [
+  {
+    id: 1,
+    question: "What is a Bitcoin?",
+    answers: [
+      {
+        text: "A) Penny",
+        correct: false,
+      },
+      {
+        text: "B) Dime",
+        correct: false,
+      },
+      {
+        text: "C) Quater",
+        correct: false,
+      },
+      {
+        text: "D) Cryptocurrency",
+        correct: true,
+      },
+    ],
+  },
+  {
+    id: 2,
+    question: "The first digital currency that allows users to send and receive money, without the interference of a central bank or government. Instead, a network of thousands of peers is controlling the transactions; a decentralized system.",
+    answers: [
+      {
+        text: "A) Dogecoin",
+        correct: false,
+      },
+      {
+        text: "B) Litecoin",
+        correct: false,
+      },
+      {
+        text: "C) Bitcoin",
+        correct: true,
+      },
+      {
+        text: "D) Shiba Inu",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 3,
+    question: "The creation of new bitcoins is also called",
+    answers: [
+      {
+        text: "A) Cooking",
+        correct: false,
+      },
+      {
+        text: "B) Mining",
+        correct: true,
+      },
+      {
+        text: "C) Baking",
+        correct: false,
+      },
+      {
+        text: "D) Sewing",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 4,
+    question: "Bitcoin was invented by an anonymous person or group named",
+    answers: [
+      {
+        text: "A) Satoshi Nakamoto",
+        correct: true,
+      },
+      {
+        text: "B) Pirates of Carribean",
+        correct: false,
+      },
+      {
+        text: "C) Wolf of Wall Street",
+        correct: false,
+      },
+      {
+        text: "D) Elon Mask",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 5,
+    question: "Due to its scarcity, Bitcoin is often nicknamed",
+    answers: [
+      {
+        text: "A) Digital Gas",
+        correct: false,
+      },
+      {
+        text: "B) Digital Coal",
+        correct: false,
+      },
+      {
+        text: "C) Digital Gold",
+        correct: true,
+      },
+      {
+        text: "D) Digital Forest",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 6,
+    question: "Database, the so-called ‘ledger’, that consists of bitcoin transaction records.",
+    answers: [
+      {
+        text: "A) Bitcoin blockchain",
+        correct: true,
+      },
+      {
+        text: "B) Bitcoin district chain",
+        correct: false,
+      },
+      {
+        text: "C) Bitcoin golden chain",
+        correct: false,
+      },
+      {
+        text: "D) Bitcoin silver chain",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 7,
+    question: "A finite supply of Bitcoin available is",
+    answers: [
+      {
+        text: "A) 1000 bitcoin",
+        correct: false,
+      },
+      {
+        text: "B) 10000 bitcoin",
+        correct: false,
+      },
+      {
+        text: "C) 1000000 bitcoin",
+        correct: false,
+      },
+      {
+        text: "D) 21 million bitcoin",
+        correct: true,
+      },
+    ],
+  },
+  {
+    id: 8,
+    question: "Besides being decentralized, borderless, secure, Bitcoin has also",
+    answers: [
+      {
+        text: "A) No inflation",
+        correct: true,
+      },
+      {
+        text: "B) Proration",
+        correct: false,
+      },
+      {
+        text: "C) No market capitalization",
+        correct: false,
+      },
+      {
+        text: "D) No Ranking",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 9,
+    question: "The world’s second-most valued cryptocurrency platform, that allows developers to deploy all sorts of decentralized applications, without interference of third parties.",
+    answers: [
+      {
+        text: "A) Etherium",
+        correct: true,
+      },
+      {
+        text: "B) Dogecoin",
+        correct: false,
+      },
+      {
+        text: "C) Uniswap",
+        correct: false,
+      },
+      {
+        text: "D) Avalanche",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 10,
+    question: "Who is the inventor of Ethereum?",
+    answers: [
+      {
+        text: "A) Bill Gates",
+        correct: false,
+      },
+      {
+        text: "B) Elon Mask",
+        correct: false,
+      },
+      {
+        text: "C) Vitalik Buterin",
+        correct: true,
+      },
+      {
+        text: "D) Satoshi Nakamoto",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 11,
+    question: "High-performance cryptocurrency blockchain which supports smart contracts and decentralized applications. It uses a proof of stake consensus mechanism with a low barrier to entry along with timestamped transactions to maximize efficiency.",
+    answers: [
+      {
+        text: "A) Solana",
+        correct: true,
+      },
+      {
+        text: "B) Bitcoin",
+        correct: false,
+      },
+      {
+        text: "C) Dogecoin",
+        correct: false,
+      },
+      {
+        text: "D) Avalanche",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 12,
+    question: "Compared to Bitcoin’s 7 TPS and Ethereum’s 15 TPS, how many transaction per second can Solana process?",
+    answers: [
+      {
+        text: "A) 100 TPS",
+        correct: false,
+      },
+      {
+        text: "B) 1000 TPS",
+        correct: false,
+      },
+      {
+        text: "C) 10 000 TPS",
+        correct: false,
+      },
+      {
+        text: "D) 50-65 000 TPS",
+        correct: true,
+      },
+    ],
+  },
+  {
+    id: 13,
+    question: "It was designed to build a bridge between cryptocurrencies and fiat currencies, with the benefits of cross-border payments facilitated by blockchain technology. Its purpose is to provide a token that represents a fiat currency at a 1:1 ratio, with in this case US dollar.",
+    answers: [
+      {
+        text: "A) Tether USD",
+        correct: true,
+      },
+      {
+        text: "B) Polcadot",
+        correct: false,
+      },
+      {
+        text: "C) HEX",
+        correct: false,
+      },
+      {
+        text: "D) Avalanche",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 14,
+    question: "Famous internet meme turned into a full-fledged cryptocurrency. Highly promoted on Twitter by Elon Mask. It is now described as a decentralized, peer-to-peer digital currency that aims to enable users to easily send money online.",
+    answers: [
+      {
+        text: "A) Dogecoin",
+        correct: true,
+      },
+      {
+        text: "B) Binance Coin",
+        correct: false,
+      },
+      {
+        text: "C) Cardano",
+        correct: false,
+      },
+      {
+        text: "D) Terra",
+        correct: false,
+      },
+    ],
+  },
+  {
+    id: 15,
+    question: "Decentralised public blockchain and cryptocurrency project, which is built in the secure Haskell programming language and fully open source. It was launched in 2017, and founded by Charles Hoskinson, co-founder of Ethereum",
+    answers: [
+      {
+        text: "A) Dogecoin",
+        correct: false,
+      },
+      {
+        text: "B) Cardano",
+        correct: true,
+      },
+      {
+        text: "C) Terra",
+        correct: false,
+      },
+      {
+        text: "D) Bitcoin",
+        correct: false,
+      },
+    ],
+  },
+];
